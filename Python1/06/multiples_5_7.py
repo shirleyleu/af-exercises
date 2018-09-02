4c2ebe61b4b4286c579439cda3abc54e78dce359
Shirley Leu <shirley.leu@schibsted.com>
2018-09-02 13:05:14 +0200

    Create a function

--- Python1/06/multiples_5_7.py
+++ Python1/06/multiples_5_7.py
@@ -1,10 +1,13 @@
-result = []
+# Function multiples_5_7 returns a list of numbers between [77, 777] which are divisible by 7 but not 5
+def multiples_5_7():
+    result = []
+    for n in range(77, 778):
+        if n % 5 == 0:
+            continue
+        elif n % 7 == 0:
+            result.append(n)
+    return result
 
-for n in range(77, 778):
-    if n % 5 == 0:
-        continue
-    elif n % 7 == 0:
-        result.append(n)
 
 if __name__ == '__main__':
-    print(result)
+    print(multiples_5_7())
